644cd738603e560b138cfb7fc0136ad017e6ee4c
“xyssky <78572737@qq.com>
2019-06-05 16:02:11 +0800

    first commit

--- static/js/csrftoken.js
+++ static/js/csrftoken.js
@@ -21,7 +21,7 @@ $.ajaxSetup({
             return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
         }
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            var csrftoken = getCookie('bk-earlybird_csrftoken');
+            var csrftoken = getCookie('bluekinglab_csrftoken');
             xhr.setRequestHeader("X-CSRFToken", csrftoken);
         }
     }
